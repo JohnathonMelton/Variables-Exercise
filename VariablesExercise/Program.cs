bfde30831ee56db9edcbdda5c48289bf338435f8
Johnathon Melton <seventh.acoustic@gmail.com>
2023-04-21 18:31:09 -0500

    Used Variables to make a story

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declare and initialize variables
+string name = "Johnathon";
+int age = 33;
+char middleFiller = '@';
+bool isMarried = true;
+double ovenTemp = 350.5;
+decimal foodPrice = 21.64m;
+
+Console.WriteLine($"My name is {name}.");
+Console.WriteLine($"I am {age} years old.");
+Console.WriteLine($"I like to spend time with my family {middleFiller} the beach. At least when I can.");
+Console.WriteLine($"It is {isMarried}. I am married believe it or not.");
+Console.WriteLine($"My wife likes to bake cookies with the oven around {ovenTemp} degrees. That seems high, but I'm not a baker so I may be wrong.");
+Console.WriteLine($"The cakes she buys cost ${foodPrice}. Those are the good kind so spend away I say.");
+
